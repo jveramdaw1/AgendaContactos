3cbdff3951a22bf6529ba52cb90ca5e6f010f4c6
avitoriacdaw1 <avitoriacdaw1@gmail.com>
2021-03-31 10:58:35 +0200

    Personal completado y a la espera de revisión

--- src/ut7/agenda/modelo/Personal.java
+++ src/ut7/agenda/modelo/Personal.java
@@ -1,9 +1,52 @@
 package ut7.agenda.modelo;
 
-public class Personal {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-	public Personal() {
+public class Personal extends Contacto{
 
+	private LocalDate fecnac;
+	private Relacion relacion;
+	private String firma;
+	
+	public Personal(String nombre, String apellidos, String telefono, String email, LocalDate fecnac,
+			Relacion relacion) {
+		super(nombre, apellidos, telefono, email);
+		this.fecnac = fecnac;
+		this.relacion = relacion;
+		firma = "Un abrazo!!";
 	}
+	public LocalDate getFecnac() {
+		return fecnac;
+	}
+	public void setFecnac(LocalDate fecnac) {
+		this.fecnac = fecnac;
+	}
+	public Relacion getRelacion() {
+		return relacion;
+	}
+	public void setRelacion(Relacion relacion) {
+		this.relacion = relacion;
+	}
+	
+	public String getFirma() {
+		return firma;
+	}
+	
+	public boolean esCumpleaños() {
+		return fecnac==LocalDate.now();
+	}
+	
+	public String formatoFecha() {
+		return fecnac.format(DateTimeFormatter.ofPattern("dd MMM. yyyy")).toLowerCase();
+	}
+	
+	@Override
+	public String toString() {
+		return this.getApellidos() + ", " + this.getNombre() + " (PERSONAL) \nTfno: " + this.getTelefono() + " | email: " + this.getEmail() + " \nFecha nacimiento: " + this.formatoFecha() + " \nRelación: " + this.getRelacion() + " \n";
+	}
+	
+	
+	
 
 }
