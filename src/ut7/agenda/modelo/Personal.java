a02e99d1a3f3e82521f196aa480b4ebfb13840f4
avitoriacdaw1 <avitoriacdaw1@gmail.com>
2021-04-06 16:15:41 +0200

    Comentarios de Personal actualizados

--- src/ut7/agenda/modelo/Personal.java
+++ src/ut7/agenda/modelo/Personal.java
@@ -7,52 +7,52 @@ public class Personal extends Contacto{ // Padre: Contacto
 
 	// Atributos
 	private LocalDate fecnac; // Fecha nacimiento
-	private Relacion relacion; // Relacion con el propietario
+	private Relacion relacion; // Relacion con el propietario, sacado de la clase Enum Relacion
 	private String firma; // Firma fija para todos los contactos en personal
 	
 	// Constructor
 	public Personal(String nombre, String apellidos, String telefono, String email, String fecnac,
 			Relacion relacion) {
-		super(nombre, apellidos, telefono, email);
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM. yyyy");
-		this.fecnac = LocalDate.parse(fecnac,formatter); // Recibe fecha nacimiento en string, pasar a LocalDate
-		this.relacion = relacion;
-		firma = "Un abrazo!!";
+		super(nombre, apellidos, telefono, email); // Del super
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM. yyyy"); // Formato que debería estar el String
+		this.fecnac = LocalDate.parse(fecnac,formatter); // Recibe fecha nacimiento en String, pasar a LocalDate
+		this.relacion = relacion; // Añade relación
+		firma = "Un abrazo!!"; // Firma fija
 	}
 	
 	//Getters y setters
-	public LocalDate getFecnac() {
+	public LocalDate getFecnac() { // Getter de fecha de nacimiento
 		return fecnac;
 	}
-	public void setFecnac(LocalDate fecnac) {
+	public void setFecnac(LocalDate fecnac) { // Setter de fecha de nacimiento
 		this.fecnac = fecnac;
 	}
-	public Relacion getRelacion() {
+	public Relacion getRelacion() { // Getter de relacion
 		return relacion;
 	}
-	public void setRelacion(Relacion relacion) {
+	public void setRelacion(Relacion relacion) { // Setter de relacion
 		this.relacion = relacion;
 	}
 	
-	public String getFirmaEmail() {
+	public String getFirmaEmail() { // Getter de firma
 		return firma;
 	}
 	// Ya que firma no requiere cambios no tiene setter
 	
 	// comprobar si es su cumpleaños
 	public boolean esCumpleaños() {
-		return fecnac==LocalDate.now();
+		return fecnac==LocalDate.now(); //Compara con dia actual
 	}
 	
 	// para pasar la fecha de nacimiento al string de patrón especificado
 	public String formatoFecha() {
-		return fecnac.format(DateTimeFormatter.ofPattern("dd MMM. yyyy")).toLowerCase();
+		return fecnac.format(DateTimeFormatter.ofPattern("dd MMM. yyyy")).toLowerCase(); // Pasa la fecha a String con el patrón especificado y en minúsculas
 	}
 	
 	// Añade al toString padre los atributo de Personal
 	@Override
 	public String toString() {
-		return super.toString() + " \nFecha nacimiento: " + this.formatoFecha() + " \nRelación: " + this.getRelacion() + " \n";
+		return super.toString() + " \nFecha nacimiento: " + this.formatoFecha() + " \nRelación: " + this.getRelacion() + " \n"; // Añade fecha nacimiento y relación
 	}
 	
 	
